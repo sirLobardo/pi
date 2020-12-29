02646cdec8af5cbc1670c3b6a59e1371a2a703e2
sirLobardo <61591862+sirLobardo@users.noreply.github.com>
2020-12-29 18:06:50 -0300

    Update pi.py

--- pi.py
+++ pi.py
@@ -14,9 +14,7 @@ a = int(input("Type the number of precision: "))
 while k < a:
     sum = ((factorial(4 * k) * (1103 + 26390 * k)) / (pow(factorial(k), 4) * pow(396, 4 * k))) + sum
     k = k + 1
-
-print(sum)
-print('{:.100}'.format(sqrt(2)))
+    
 pi = (9801) / (2 * sqrt(2) * sum)
 print('{:.100}'.format(pi))
 
